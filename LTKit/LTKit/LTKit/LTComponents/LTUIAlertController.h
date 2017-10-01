d59b04d3fb749e6f24dc6e92de36e18e26fc81af
孟令通 <ak-7755@qq.com>
2017-10-01 23:48:10 +0800

    alert Controller init

---
+++ LTKit/LTKit/LTKit/LTComponents/LTUIAlertController.h
@@ -0,0 +1,13 @@
+//
+//  LTUIAlertController.h
+//  LTKit
+//
+//  Created by 孟令通 on 2017/10/1.
+//  Copyright © 2017年 LryMlt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LTUIAlertController : UIViewController
+
+@end
---
+++ LTKit/LTKit/LTKit/LTComponents/LTUIAlertController.m
@@ -0,0 +1,58 @@
+//
+//  LTUIAlertController.m
+//  LTKit
+//
+//  Created by 孟令通 on 2017/10/1.
+//  Copyright © 2017年 LryMlt. All rights reserved.
+//
+
+#import "LTUIAlertController.h"
+
+@interface LTUIAlertController ()
+
+@end
+
+@implementation LTUIAlertController
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
+        [self didInitialized];
+    }
+    return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+    if (self = [super initWithCoder:aDecoder]) {
+        [self didInitialized];
+    }
+    return self;
+}
+
+- (void)didInitialized
+{
+    
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
