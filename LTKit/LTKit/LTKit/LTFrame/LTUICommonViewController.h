6c74d5abcb4a372008b9a2f6e34268f2a4f4619d
孟令通 <ak-7755@qq.com>
2017-10-01 17:59:15 +0800

    CommonViewController

---
+++ LTKit/LTKit/LTKit/LTFrame/LTUICommonViewController.h
@@ -0,0 +1,13 @@
+//
+//  LTUICommonViewController.h
+//  LTKit
+//
+//  Created by 孟令通 on 2017/10/1.
+//  Copyright © 2017年 LryMlt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LTUICommonViewController : UIViewController
+
+@end
